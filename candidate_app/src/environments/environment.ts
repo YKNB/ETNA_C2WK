f9de60b9bbb555dcdd17dcf42f1703f269a761d6
Karlito <karlo02@live.fr>
2022-05-05 11:08:37 +0200

    file environnement

---
+++ candidate_app/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAPlUgQLahnnb7y_05rDThqkwRDw33oqts",
+    authDomain: "angular-2022-65011.firebaseapp.com",
+    databaseURL: "https://angular-2022-65011-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "angular-2022-65011",
+    storageBucket: "angular-2022-65011.appspot.com",
+    messagingSenderId: "725517257665",
+    appId: "1:725517257665:web:2ef0b237f81d553745bd40",
+    measurementId: "G-H8GT50FEEB"
+  }
+};
