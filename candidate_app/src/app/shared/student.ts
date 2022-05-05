e9a76bffbba5bd52706fb92b83e88b71c8770f81
Karlito <karlo02@live.fr>
2022-05-05 11:14:41 +0200

    Add files via upload

---
+++ candidate_app/src/app/shared/student.ts
@@ -0,0 +1,10 @@
+export interface Student {
+  $key: string;
+  firstName: string;
+  lastName: string;
+  competence: string;
+  exp: string;
+  domaine: string;
+  email: string;
+  mobileNumber: Number;
+}
